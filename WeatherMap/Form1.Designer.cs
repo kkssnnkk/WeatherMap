22d9e56472954469fbc4e2c0d263b16eb9f890d3
Bohdan <bogdantelep@gmail.com>
2022-09-29 00:45:56 +0300

    Adding the worldmap

--- WeatherMap/Form1.Designer.cs
+++ WeatherMap/Form1.Designer.cs
@@ -28,13 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(12, 12);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(541, 426);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 0D;
+            this.map.Load += new System.EventHandler(this.map_Load);
+            this.map.MouseClick += new System.Windows.Forms.MouseEventHandler(this.map_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.map);
+            this.Name = "Form1";
+            this.Text = "WeatherMap";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
     }
 }
 
--- WeatherMap/Form1.cs
+++ WeatherMap/Form1.cs
@@ -7,14 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms.ToolTips;
+using GMap.NET.ObjectModel;
 
 namespace WeatherMap
 {
     public partial class Form1 : Form
     {
+        public GMapOverlay points = new GMapOverlay("points"); // метки на карте
+        public GMapMarker point; // метка
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void map_Load(object sender, EventArgs e)
+        {
+            map.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            map.MinZoom = 2;
+            map.MaxZoom = 16;
+            map.Zoom = 10;
+            map.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
+            map.CanDragMap = true;
+            map.DragButton = MouseButtons.Left;
+            map.ShowCenter = false;
+            map.ShowTileGridLines = false;
+            map.Overlays.Add(points); // отображение на карте меток из списка points
+            setPointer(getCoords("Zaporizhzhia, Ukraine")); // устанавливаем метку
+        }
+
+        private void setPointer(PointLatLng point)
+        {
+            if (points.Markers.Count() > 0) // если меток больше чем одна
+                points.Markers.Clear();
+
+            this.point = new GMarkerGoogle(point, GMarkerGoogleType.red_dot); // создаём новую метку
+            this.point.ToolTipText = $"Lat: {point.Lat}\nLng: {point.Lng}"; // текст метки
+            points.Markers.Add(this.point); // закидываем метку в список меток
+            map.Position = point; // центрируем карту
+        }
+
+        private PointLatLng getCoords(string q) // вернёт координаты метки по опр. запросу
+        {
+            GMapControl m = new GMapControl();
+            m.SetPositionByKeywords(q);
+            PointLatLng point = new PointLatLng(m.Position.Lat, m.Position.Lng);
+            return point;
+        }
+
+        private void map_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Middle)
+            {
+                PointLatLng point = map.FromLocalToLatLng(e.X, e.Y); // создаём координаты метки 
+                setPointer(point); // устанавливаем новую метку
+            }
         }
     }
 }
