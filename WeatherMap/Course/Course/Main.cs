3e58636773ee91f026927509ce61817830db05cd
IrinaGorpinich <107684932+IrinaGorpinich@users.noreply.github.com>
2022-10-02 15:32:42 +0300

    Add files via upload

---
+++ WeatherMap/Course/Course/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json.Linq;
+
+namespace Course
+{
+    public partial class Main
+    {
+        public TemperatureObj Temperature { get; }
+
+        public double Pressure { get; }
+
+        public double Humidity { get; }
+
+        public double SeaLevelAtm { get; }
+
+        public double GroundLevelAtm { get; }
+
+        public Main(JToken mainData)
+        {
+            if (mainData is null)
+                throw new System.ArgumentNullException(nameof(mainData));
+
+            Temperature = new TemperatureObj(
+                 double.Parse(mainData.SelectToken("temp").ToString(), CultureInfo.InvariantCulture),
+                 double.Parse(mainData.SelectToken("temp_min").ToString(), CultureInfo.InvariantCulture),
+                 double.Parse(mainData.SelectToken("temp_max").ToString(), CultureInfo.InvariantCulture));
+
+            Pressure = double.Parse(mainData.SelectToken("pressure").ToString(), CultureInfo.InvariantCulture);
+            Humidity = double.Parse(mainData.SelectToken("humidity").ToString(), CultureInfo.InvariantCulture);
+            if (mainData.SelectToken("sea_level") != null)
+                SeaLevelAtm = double.Parse(mainData.SelectToken("sea_level").ToString(), CultureInfo.InvariantCulture);
+            if (mainData.SelectToken("grnd_level") != null)
+                GroundLevelAtm = double.Parse(mainData.SelectToken("grnd_level").ToString(), CultureInfo.InvariantCulture);
+        }
+    }
+}
