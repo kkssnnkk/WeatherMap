89cd1f2a86bb9ae5d7a65594dca729f1a293c502
Bohdan <bogdantelep@gmail.com>
2022-10-11 15:33:33 +0300

    Updating api calls class

--- WeatherMap/ApiCalls.cs
+++ WeatherMap/ApiCalls.cs
@@ -6,13 +6,23 @@ namespace WeatherMap
 {
     public class ApiCalls
     {
-        private string access_key = "";
-        private string base_url = "http://api.weatherstack.com/current?access_key=";
+        private string access_key = ""; // api provider access key
+        private string base_url = "http://api.weatherstack.com/current?access_key="; // api base 
         private HttpClient client = new HttpClient();
 
-        public string getJsonResponseString(string query) 
+        // get weather data by city name
+        public string getJsonResponseString(string city) 
         {
-            client.BaseAddress = new Uri(base_url + access_key + "&query=" + query);
+            client.BaseAddress = new Uri(base_url + access_key + "&query=" + city); // change this request string in order to use new api
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress).Result;
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        // get weather data by coords
+        public string getJsonResponseStringByCoords(float lat, float lon) 
+        {
+            client.BaseAddress = new Uri(base_url + access_key + "&query=" + lat + ',' + lon); // change this request string in order to use new api
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             HttpResponseMessage response = client.GetAsync(client.BaseAddress).Result;
             return response.Content.ReadAsStringAsync().Result;
